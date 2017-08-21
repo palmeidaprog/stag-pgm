b41b46ea8aa63a1be9e607ff82d084d7ebbfbdfb
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2017-08-21 07:07:46 -0300

    [Sync Save] Editing UI ...

--- src/com/github/palmeidaprog/stagpgm/main/Main.java
+++ src/com/github/palmeidaprog/stagpgm/main/Main.java
@@ -17,8 +17,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Esteganografia com Imagens PGM");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
